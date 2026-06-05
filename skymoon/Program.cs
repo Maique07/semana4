30ab0ed9d265fa7dfa3b297c7a7d6ba1e2d739fb
Maique07 <152615451+Maique07@users.noreply.github.com>
2026-06-05 00:22:27 +0000

    fazendo os outros metodos

--- skymoon/Program.cs
+++ skymoon/Program.cs
@@ -63,22 +63,60 @@ app.MapGet("/funcionario", () =>
             funcionarioCadastrados
 });
 });
-/*
+
 app.MapPatch("/funcionario/{id}", (int id, JsonElement body) =>
 {
+    double novo_salario = body.GetProperty("salario").GetDouble();
+
+    for(int i=0; i<totalFuncionarios; i++){
+         if(funcionarios[i].Id == id){
+            funcionarios[i].Salario = novo_salario;
+            
+            return Results.Ok(new{ funcionario = funcionarios[i]});
+         }
+    }
+     return Results.NotFound(new {mensage = "funcionario não encontrado"});
+
     
 });
 
+
+
 app.MapPut("/funcionario/{id}", (int id, JsonElement body) =>
 {   
-    
+     for(int i=0; i<totalFuncionarios; i++){
+         if(funcionarios[i].Id == id){
+             funcionarios[i].Nome = body.GetProperty("nome").GetString();
+             funcionarios[i].Idade = body.GetProperty("idade").GetInt32();
+             funcionarios[i].Cargo = body.GetProperty("cargo").GetString();
+             funcionarios[i].Departamento = body.GetProperty("departamento").GetString();
+             funcionarios[i].Salario = body.GetProperty("salario").GetDouble();
+             
+              return Results.Ok(new{ funcionarios});
+         }
+     }
+      return Results.NotFound(new {mensage = "funcionario não encontrado"});
 });
 
 app.MapDelete("/funcionario", (int id) =>
 {
-    
+     for(int i=0; i<totalFuncionarios; i++){
+         if(funcionarios[i].Id == id){
+            Funcionario funcionario_removido = funcionarios[i];
+              for(int x = i; x < totalFuncionarios; x++){
+                      funcionarios[x] = funcionarios[x+1];   
+              }
+              totalFuncionarios--;
+               return Results.Ok(new
+            {
+                mensagem = "Funcionário removido com sucesso.",
+                funcionario = funcionario_removido
+            });
+         }
+     }
+        return Results.NotFound(new {mensage = "funcionario não encontrado"});
 });
-
+/*
 app.MapGet("/funcionario/departamento/busca", (string departamento) =>
 {
     
